b2a19b1beffc2b28db4afbb239f8f70163506db7
Eiji MATSUMOTO <e.mattsan@gmail.com>
2018-12-03 06:43:47 +0900

    implement min edit distance algorithm

--- ch3/min_edit_distance/lib/min_edit_distance.ex
+++ ch3/min_edit_distance/lib/min_edit_distance.ex
@@ -3,16 +3,53 @@ defmodule MinEditDistance do
   Documentation for MinEditDistance.
   """
 
-  @doc """
-  Hello world.
+  defmodule Matrix do
+    defstruct row_size: 0, col_size: 0, entities: nil
 
-  ## Examples
+    def new(row_size, col_size) do
+      entities = :array.new(row_size * col_size)
+      %Matrix{row_size: row_size, col_size: col_size, entities: entities}
+    end
 
-      iex> MinEditDistance.hello()
-      :world
+    def set(%Matrix{row_size: row_size, col_size: col_size, entities: entities} = matrix, row, col, value) when row < row_size do
+      # IO.inspect [row, col, row * col_size + col, value, entities]
+      %{matrix | entities: :array.set(row * col_size + col, value, entities)}
+    end
 
-  """
-  def hello do
-    :world
+    def get(%Matrix{row_size: row_size, col_size: col_size, entities: entities}, row, col) when row < row_size do
+      :array.get(row * col_size + col, entities)
+    end
+
+    def to_list(%Matrix{col_size: col_size, entities: entities}) do
+      entities
+      |> :array.to_list()
+      |> Enum.chunk_every(col_size)
+    end
   end
+
+  alias MinEditDistance.Matrix
+
+  def min_edit_distance(s1, s2) do
+    cs1 = s1 |> String.to_charlist() |> :array.from_list()
+    cs2 = s2 |> String.to_charlist() |> :array.from_list()
+    len1 = :array.size(cs1)
+    len2 = :array.size(cs2)
+
+    m = Matrix.new(len1 + 1, len2 + 1)
+    m = 0..len1 |> Enum.reduce(m, &Matrix.set(&2, &1, 0, &1))
+    m = 0..len2 |> Enum.reduce(m, &Matrix.set(&2, 0, &1, &1))
+
+    indices = for i <- 1..len1, j <- 1..len2, do: {i, j}
+
+    indices
+    |> Enum.reduce(m, fn {i, j}, m ->
+      cost = if :array.get(i - 1, cs1) == :array.get(j - 1, cs2), do: 0, else: 1
+      replaceCost = Matrix.get(m, i - 1, j - 1) + cost
+      removeCost = Matrix.get(m, i - 1, j) + 1
+      insertCost = Matrix.get(m, i, j - 1) + 1
+      Matrix.set(m, i, j, Enum.min([replaceCost, removeCost, insertCost]))
+    end)
+    |> Matrix.to_list()
+  end
+
 end
