606a22e92c0218abf001abeb3e784f629ae6d50a
MATSUMOTO EIji <e-matsumoto@esm.co.jp>
2018-12-03 15:44:50 +0900

    add basic test cases: same length

--- ch3/min_edit_distance/test/min_edit_distance_test.exs
+++ ch3/min_edit_distance/test/min_edit_distance_test.exs
@@ -2,7 +2,36 @@ defmodule MinEditDistanceTest do
   use ExUnit.Case
   doctest MinEditDistance
 
-  test "greets the world" do
-    assert MinEditDistance.hello() == :world
+  describe "same length" do
+    test "distance between ABC and ABC" do
+      assert MinEditDistance.min_edit_distance("ABC", "ABC") == [
+               [0, 1, 2, 3],
+               [1, 0, 1, 2],
+               [2, 1, 0, 1],
+               [3, 2, 1, 0]
+             ]
+    end
+
+    test "distance between ABC and XYZ" do
+      assert MinEditDistance.min_edit_distance("ABC", "XYZ") == [
+               [0, 1, 2, 3],
+               [1, 1, 2, 3],
+               [2, 2, 2, 3],
+               [3, 3, 3, 3]
+             ]
+    end
+
+    test "distance between ABC and ACE" do
+      assert MinEditDistance.min_edit_distance("ABC", "ACE") == [
+               [0, 1, 2, 3],
+               [1, 0, 1, 2],
+               [2, 1, 1, 2],
+               [3, 2, 1, 2]
+             ]
+    end
+
+    test "distance between blank and blank" do
+      assert MinEditDistance.min_edit_distance("", "") == [[0]]
+    end
   end
 end
