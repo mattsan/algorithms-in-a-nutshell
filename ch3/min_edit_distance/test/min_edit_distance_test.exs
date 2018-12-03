03ef4513e6ead229602dfc283ce595ab22715b60
Eiji MATSUMOTO <e.mattsan@gmail.com>
2018-12-03 21:43:20 +0900

    simplify assertions

--- ch3/min_edit_distance/test/min_edit_distance_test.exs
+++ ch3/min_edit_distance/test/min_edit_distance_test.exs
@@ -2,25 +2,27 @@ defmodule MinEditDistanceTest do
   use ExUnit.Case
   doctest MinEditDistance
 
+  import MinEditDistance
+
   describe "blank" do
     test "distance between blank and blank" do
-      assert MinEditDistance.min_edit_distance("", "") == [[0]]
+      assert min_edit_distance("", "") == [[0]]
     end
   end
 
   describe "one side is an blank" do
     test "distance between ABC and blank" do
-      assert MinEditDistance.min_edit_distance("ABC", "") == [[0], [1], [2], [3]]
+      assert min_edit_distance("ABC", "") == [[0], [1], [2], [3]]
     end
 
     test "distance between blank andd ABC" do
-      assert MinEditDistance.min_edit_distance("", "ABC") == [[0, 1, 2, 3]]
+      assert min_edit_distance("", "ABC") == [[0, 1, 2, 3]]
     end
   end
 
   describe "same length" do
     test "distance between ABC and ABC" do
-      assert MinEditDistance.min_edit_distance("ABC", "ABC") == [
+      assert min_edit_distance("ABC", "ABC") == [
                [0, 1, 2, 3],
                [1, 0, 1, 2],
                [2, 1, 0, 1],
@@ -29,7 +31,7 @@ defmodule MinEditDistanceTest do
     end
 
     test "distance between ABC and XYZ" do
-      assert MinEditDistance.min_edit_distance("ABC", "XYZ") == [
+      assert min_edit_distance("ABC", "XYZ") == [
                [0, 1, 2, 3],
                [1, 1, 2, 3],
                [2, 2, 2, 3],
@@ -38,7 +40,7 @@ defmodule MinEditDistanceTest do
     end
 
     test "distance between ABC and ACE" do
-      assert MinEditDistance.min_edit_distance("ABC", "ACE") == [
+      assert min_edit_distance("ABC", "ACE") == [
                [0, 1, 2, 3],
                [1, 0, 1, 2],
                [2, 1, 1, 2],
@@ -49,7 +51,7 @@ defmodule MinEditDistanceTest do
 
   describe "general cases" do
     test "distance between ABC and CDEF" do
-      assert MinEditDistance.min_edit_distance("ABC", "CDEF") == [
+      assert min_edit_distance("ABC", "CDEF") == [
                [0, 1, 2, 3, 4],
                [1, 1, 2, 3, 4],
                [2, 2, 2, 3, 4],
@@ -58,7 +60,7 @@ defmodule MinEditDistanceTest do
     end
 
     test "distance between Phoenix and Elixir" do
-      assert MinEditDistance.min_edit_distance("Phoenix", "Elixir") == [
+      assert min_edit_distance("Phoenix", "Elixir") == [
                [0, 1, 2, 3, 4, 5, 6],
                [1, 1, 2, 3, 4, 5, 6],
                [2, 2, 2, 3, 4, 5, 6],
