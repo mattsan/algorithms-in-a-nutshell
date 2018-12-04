296e942ca9a3db2bda4b50565ae16cd7ffc5c246
Eiji MATSUMOTO <e.mattsan@gmail.com>
2018-12-04 21:16:09 +0900

    add min-edit-operations

--- ch3/min_edit_distance/lib/min_edit_distance.ex
+++ ch3/min_edit_distance/lib/min_edit_distance.ex
@@ -37,6 +37,51 @@ defmodule MinEditDistance do
   alias MinEditDistance.Matrix
 
   def min_edit_distance(s1, s2) do
+    min_edit_distance_matrix(s1, s2)
+    |> List.last()
+    |> List.last()
+  end
+
+  def min_edit_distance_matrix(s1, s2) do
+    min_edit_distance_and_operations(s1, s2) |> elem(0)
+  end
+
+  def min_edit_operations(s1, s2) do
+    {ds, os} = min_edit_distance_and_operations(s1, s2)
+
+    Enum.zip(ds, os)
+    |> Enum.reverse()
+    |> Enum.map(fn {d, o} ->
+      Enum.zip(d, o) |> Enum.reverse()
+    end)
+    |> parse([])
+    |> Enum.with_index(1)
+    |> Enum.reject(fn
+      {:nop, _} -> true
+      _ -> false
+    end)
+  end
+
+  defp parse([], acc), do: acc
+  defp parse([[{_, :undefined}] | _], acc), do: acc
+
+  defp parse([[{_, :remove} | _] | rest_row], acc) do
+    parse(rest_row, [:remove | acc])
+  end
+
+  defp parse([[{_, :insert} | rest_col] | rest_row], acc) do
+    parse([rest_col | Enum.map(rest_row, &Enum.drop(&1, 1))], [:insert | acc])
+  end
+
+  defp parse([ [{d, _} | _], [_, {d, _} = c | rest_col] | rest_row], acc) do
+    parse([[c | rest_col] | Enum.map(rest_row, &Enum.drop(&1, 1))], [:nop | acc])
+  end
+
+  defp parse([_ | rest], acc) do
+    parse(Enum.map(rest, &Enum.drop(&1, 1)), [:replace | acc])
+  end
+
+  defp min_edit_distance_and_operations(s1, s2) do
     cs1 = s1 |> String.to_charlist() |> :array.from_list()
     cs2 = s2 |> String.to_charlist() |> :array.from_list()
     len1 = :array.size(cs1)
@@ -45,19 +90,24 @@ defmodule MinEditDistance do
     m = Matrix.new(len1 + 1, len2 + 1)
     m = 0..len1 |> Enum.reduce(m, &Matrix.set(&2, &1, 0, &1))
     m = 0..len2 |> Enum.reduce(m, &Matrix.set(&2, 0, &1, &1))
+    ops = Matrix.new(len1 + 1, len2 + 1)
 
     range1 = if len1 > 0, do: 1..len1, else: []
     range2 = if len2 > 0, do: 1..len2, else: []
     indices = for i <- range1, j <- range2, do: {i, j}
 
-    indices
-    |> Enum.reduce(m, fn {i, j}, m ->
-      cost = if :array.get(i - 1, cs1) == :array.get(j - 1, cs2), do: 0, else: 1
-      replaceCost = Matrix.get(m, i - 1, j - 1) + cost
-      removeCost = Matrix.get(m, i - 1, j) + 1
-      insertCost = Matrix.get(m, i, j - 1) + 1
-      Matrix.set(m, i, j, Enum.min([replaceCost, removeCost, insertCost]))
-    end)
-    |> Matrix.to_list()
+    {distance, ops} =
+      indices
+      |> Enum.reduce({m, ops}, fn {i, j}, {m, ops} ->
+        cost = if :array.get(i - 1, cs1) == :array.get(j - 1, cs2), do: 0, else: 1
+        replaceCost = Matrix.get(m, i - 1, j - 1) + cost
+        removeCost = Matrix.get(m, i - 1, j) + 1
+        insertCost = Matrix.get(m, i, j - 1) + 1
+        {min, op} = [replaceCost, removeCost, insertCost] |> Enum.zip([:replace, :remove, :insert]) |> Enum.min()
+
+        {Matrix.set(m, i, j, min), Matrix.set(ops, i, j, op)}
+      end)
+
+    {Matrix.to_list(distance), Matrix.to_list(ops)}
   end
 end
--- ch3/min_edit_distance/test/min_edit_distance_test.exs
+++ ch3/min_edit_distance/test/min_edit_distance_test.exs
@@ -6,23 +6,23 @@ defmodule MinEditDistanceTest do
 
   describe "blank" do
     test "distance between blank and blank" do
-      assert min_edit_distance("", "") == [[0]]
+      assert min_edit_distance_matrix("", "") == [[0]]
     end
   end
 
   describe "one side is an blank" do
     test "distance between ABC and blank" do
-      assert min_edit_distance("ABC", "") == [[0], [1], [2], [3]]
+      assert min_edit_distance_matrix("ABC", "") == [[0], [1], [2], [3]]
     end
 
     test "distance between blank andd ABC" do
-      assert min_edit_distance("", "ABC") == [[0, 1, 2, 3]]
+      assert min_edit_distance_matrix("", "ABC") == [[0, 1, 2, 3]]
     end
   end
 
   describe "same length" do
     test "distance between ABC and ABC" do
-      assert min_edit_distance("ABC", "ABC") == [
+      assert min_edit_distance_matrix("ABC", "ABC") == [
                [0, 1, 2, 3],
                [1, 0, 1, 2],
                [2, 1, 0, 1],
@@ -31,7 +31,7 @@ defmodule MinEditDistanceTest do
     end
 
     test "distance between ABC and XYZ" do
-      assert min_edit_distance("ABC", "XYZ") == [
+      assert min_edit_distance_matrix("ABC", "XYZ") == [
                [0, 1, 2, 3],
                [1, 1, 2, 3],
                [2, 2, 2, 3],
@@ -40,7 +40,7 @@ defmodule MinEditDistanceTest do
     end
 
     test "distance between ABC and ACE" do
-      assert min_edit_distance("ABC", "ACE") == [
+      assert min_edit_distance_matrix("ABC", "ACE") == [
                [0, 1, 2, 3],
                [1, 0, 1, 2],
                [2, 1, 1, 2],
@@ -51,7 +51,7 @@ defmodule MinEditDistanceTest do
 
   describe "general cases" do
     test "distance between ABC and CDEF" do
-      assert min_edit_distance("ABC", "CDEF") == [
+      assert min_edit_distance_matrix("ABC", "CDEF") == [
                [0, 1, 2, 3, 4],
                [1, 1, 2, 3, 4],
                [2, 2, 2, 3, 4],
@@ -60,7 +60,7 @@ defmodule MinEditDistanceTest do
     end
 
     test "distance between Phoenix and Elixir" do
-      assert min_edit_distance("Phoenix", "Elixir") == [
+      assert min_edit_distance_matrix("Phoenix", "Elixir") == [
                [0, 1, 2, 3, 4, 5, 6],
                [1, 1, 2, 3, 4, 5, 6],
                [2, 2, 2, 3, 4, 5, 6],
