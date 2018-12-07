9bc4df769cc19a94301e8cd9995bba6f46410124
Eiji MATSUMOTO <e.mattsan@gmail.com>
2018-12-07 21:02:24 +0900

    cosmetic heap sort tests

--- ch4/ruby/heap_sort_test.rb
+++ ch4/ruby/heap_sort_test.rb
@@ -3,26 +3,34 @@ require './heap_sort'
 
 class TestHeapSort < Minitest::Test
   def test_sorting_empty_array
-    assert_equal HeapSort([]), []
+    assert_equal [], HeapSort([])
   end
 
   def test_sorting_an_element_array
-    assert_equal HeapSort([1]), [1]
+    assert_equal [1], HeapSort([1])
   end
 
   def test_sorting_6_element_array
-    assert_equal HeapSort([8, 11, 9, 2, 10, 16]),  [2, 8, 9, 10, 11, 16]
+    source = [8, 11, 9, 2, 10, 16]
+    expected = [2, 8, 9, 10, 11, 16]
+    assert_equal expected, HeapSort(source)
   end
 
   def test_sorting_8_element_array
-    assert_equal HeapSort([3, 1, 4, 1, 5, 9, 2, 6, 5]), [1, 1, 2, 3, 4, 5, 5, 6, 9]
+    source = [3, 1, 4, 1, 5, 9, 2, 6, 5]
+    expected = [1, 1, 2, 3, 4, 5, 5, 6, 9]
+    assert_equal expected, HeapSort(source)
   end
 
   def test_sorting_already_sorted_array
-    assert_equal HeapSort([1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5]), [1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5]
+    source = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5]
+    expected = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5]
+    assert_equal expected, HeapSort(source)
   end
 
   def test_sorting_string_array
-    assert_equal HeapSort(%w(one two three four five)), %w(five four one three two)
+    source = %w(one two three four five)
+    expected = %w(five four one three two)
+    assert_equal expected, HeapSort(source)
   end
 end
